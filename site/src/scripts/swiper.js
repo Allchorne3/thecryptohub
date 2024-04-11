7977e2025615b65a5cb37869a2f63bc531d17e1e
Stefan Allchorne <allchorne39@sky.com>
2024-04-11 18:45:49 +0100

    Update mvp cards

--- site/src/scripts/swiper.js
+++ site/src/scripts/swiper.js
@@ -5,30 +5,25 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const setupSwiper = () => {
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
+    const swiper = new Swiper(".swiper", {
+        slidesPerView: 4,
+        spaceBetween: 0,
         loop: true,
-        effect: "cards",
-        grabCursor: true,
 
         autoplay: {
             delay: 6000,
             disableOnInteraction: false,
         },
 
-        // If we need pagination
         pagination: {
-            el: '.swiper-pagination',
+            el: ".swiper-pagination",
             clickable: true,
         },
 
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-
     });
 }
 
