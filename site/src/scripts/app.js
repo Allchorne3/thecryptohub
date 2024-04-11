6d20b39ca84686034017df1f1a1f0652dd195cce
Stefan Allchorne <allchorne39@sky.com>
2024-04-11 11:07:36 +0100

    avoid section

--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -29,7 +29,20 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     .to(document.querySelector('body'), {
-        backgroundColor: "#202922", // Set the 'd' attribute in the svg to be the value of finalPath
+        backgroundImage: "linear-gradient(90deg, #191c27, #25293d, #191c27)", // Set the 'd' attribute in the svg to be the value of finalPath
+        ease: "none" // Remove easing since the scrub will add a smooth delay
+    })
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: document.querySelector('#section-mvp'), // Use the divider as the trigger
+            start: 'top top', // Start the animation when the top of the path reaches the bottom of the viewport
+            end: 'bottom top', // End the animation when the bottom of the path reaches the top of the viewport
+            scrub: 1 // Add a delay of 1 second to the animation
+        }
+    })
+    .to(document.querySelector('body'), {
+        backgroundImage: "linear-gradient(90deg, #191c27, #401010, #191c27)", // Set the 'd' attribute in the svg to be the value of finalPath
         ease: "none" // Remove easing since the scrub will add a smooth delay
     })
 
