0061ab1a79b071875980d8c0e75b5d4148e5d116
Stefan Allchorne <allchorne39@sky.com>
2024-04-30 09:59:34 +0100

    new header and hero

--- site/src/scripts/animations.js
+++ site/src/scripts/animations.js
@@ -5,30 +5,6 @@ gsap.registerPlugin(ScrollTrigger);
 const setupAnimations = () => {
     const heroBody = document.querySelector('.hero-body')
 
-    
-    
-    if(window.matchMedia("(min-width: 768px)").matches) {
-        gsap.to(document.querySelector('.hero'), { padding: '2.5rem', duration: 1.25, ease: "power1.out" })
-        gsap.to(document.querySelector('.hero-body'), { borderRadius: '2.5rem', duration: 1.25, ease: "power1.out"})
-    }
-
-    gsap.to(document.querySelector('.hero-body img'), { opacity: '1', y: 0, duration: .6, ease: "power1.out", delay: .8})
-    gsap.to(document.querySelector('.hero-body h1'), { opacity: '1', y: 0, duration: .6, ease: "power1.out", delay: .95})
-    gsap.to(document.querySelector('.hero-body p'), { opacity: '1', y: 0, duration: .6, ease: "power1.out", delay: 1.1})
-        
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: heroBody, // Use the divider as the trigger
-            start: 'top top', // Start the animation when the top of the path reaches the bottom of the viewport
-            end: 'center 20%', // End the animation when the bottom of the path reaches the top of the viewport
-            scrub: 1 // Add a delay of 1 second to the animation
-        }
-    })
-    .to(document.querySelector('body'), {
-        backgroundImage: "linear-gradient(90deg, #191c27, #25293d, #191c27)", // Set the 'd' attribute in the svg to be the value of finalPath
-        ease: "none" // Remove easing since the scrub will add a smooth delay
-    })
-
     gsap.timeline({
         scrollTrigger: {
             trigger: document.querySelector('#section-mvp'), // Use the divider as the trigger
